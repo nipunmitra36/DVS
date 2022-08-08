116a8db4c855d111b6b9156e7e9bba24fff6e601
Nipun Mitra <nipunmitra03@gmail.com>
2022-08-09 00:43:57 +0600

    1st-commit

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// // wow js
+// new WOW().init();
+
+// owl carousel 
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+  loader.style.display="none";
+});
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
